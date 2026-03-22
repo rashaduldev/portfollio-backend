47c27f7dfa5aff2ca54e48c1708e14039b498147
Md Rashadul Islam <rashadul.dev@gmail.com>
2026-03-22 17:02:35 +0600

    fix: trust proxy added

--- src/app.ts
+++ src/app.ts
@@ -52,6 +52,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(compression());
+app.set("trust proxy", 1);
 
 // ─── Logging ─────────────────────────────────────────────────────────────────
 app.use(morgan("combined", { stream: morganStream }));
